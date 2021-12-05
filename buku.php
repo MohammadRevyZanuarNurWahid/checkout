4424a30c665fdafeee83ef26802a65af65b3bd79
MohammadRevyZanuarNurWahid <72596646+MohammadRevyZanuarNurWahid@users.noreply.github.com>
2021-12-05 21:59:29 +0700

    Add files via upload

---
+++ buku.php
@@ -0,0 +1,27 @@
+<?php
+	include "header.php";
+?>
+<h3>Daftar Buku</h3>
+<div class="row">
+	<?php
+	include "koneksi.php";
+	$qry_buku=mysqli_query($conn, "select * from buku");
+	while ($dt_buku=mysqli_fetch_array($qry_buku)) {
+	?>
+	<div class="col-md-3">
+		<div class="card">
+			<img src="buku.jpg" class="card-img-top">
+			<div class="card-body">
+				<h5 class="card-tittle"><?=$dt_buku['judul_buku']?></h5>
+				<p class="card-text"><?=substr($dt_buku['deskripsi'], 0,20)?></p>
+				<a href="pinjam_buku.php?id_buku=<?=$dt_buku['id_buku']?>" class="btn btn-primary">Pinjam</a>
+			</div>
+		</div>
+	</div>
+	<?php
+	}
+	?>
+</div>
+<?php
+	include "footer.php";
+?>
---
+++ home.php
@@ -0,0 +1,7 @@
+<?php
+	include "header.php";
+?>
+<h2>Selamat Datang <?=$_SESSION['nama']?> di website perpus online.</h2>
+<?php
+	include "footer.php";
+?>
